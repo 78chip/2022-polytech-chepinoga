f24525eaae6a54bcf72f415206a614e83ee07e41
78chip <chepinoga.aa@edu.spbstu.ru>
2022-10-08 23:17:20 +0300

    feat: mems.cpp updated

--- 05_computer_architecture/meminfo/mems.cpp
+++ 05_computer_architecture/meminfo/mems.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
 
 void show_meminfo()
 {
     char parameter[20];
-    int value;
     char unit[3];
-    int memTotal=0, memAvalable=0;
+    int memTotal, memFree, memAvalable;
     
     std::ifstream in("/proc/meminfo", std::ios_base::in);
-    while(in.good())
-    {
-        in >> parameter >> value >> unit;
-        if (std::strcmp(parameter, "MemTotal:") == 0)
-            memTotal=value;
-        if (std::strcmp(parameter, "MemAvailable:") == 0)
-            memAvalable = value;
-        // std::cout << parameter << " " << value << unit << std::endl;
+    if(in.is_open()) {
+        in >> parameter >> memTotal >> unit;
+        in >> parameter >> memFree >> unit;
+        in >> parameter >> memAvalable >> unit;
+    } else {
+        exit(1);
     }
     
-    int percent = memAvalable*100/memTotal;
-    for (int i=0;i<100;i+=10)
-        if (i<percent)
+    int percent = (memTotal - memAvalable) * 100 / memTotal;
+    for (int i = 0; i < 100; i += 10)
+        if (i < percent)
             std::cout << '|';
         else
             std::cout << '.';
-    std::cout << " " << percent << std::endl;
+    std::cout << " " << percent << "%" << std::endl;
 }
 
 int main()
@@ -34,3 +30,4 @@ int main()
   show_meminfo();
   return 0;
 }
+
