787ed0580ff9f225bf6cda31bf000d9b93cf13d8
78chip <chepinoga.aa@edu.spbstu.ru>
2022-10-21 23:10:28 +0300

    feat: palind.cpp updated

--- 06_pointers_references/palindrome/palind.cpp
+++ 06_pointers_references/palindrome/palind.cpp
@@ -1,23 +1,29 @@
-#include <iostream>
-#include <string.h>
-using namespace std;
- bool palindrom(char s[100]);
-int main()
-{   
-    char s[100];
-    cout << "Enter a line" << endl;
-    cin.getline(s, 100);
- if (palindrom(s))
-    cout << "Palindrome " << endl;
-else
-cout<<"Not a Palindrome"<<endl;
+#include <cassert>
+
+unsigned int length(const char* str) {
+  if (!str)
     return 0;
+  const char* end = str;
+  for( ; *end != '\0'; ++end)
+    ;
+  return end - str;
 }
- bool palindrom(char s[])
-    {
-      int n = strlen(s);
- for (int i = 0;  i < n / 2; i++ ) // идёт проверка слов на полиндромность,кол-во проверок = длина на 2.
-if(s[i]!=s[n-1-i])
-return false;
-return true;
+
+bool ispalindrome(const char* str) {
+  if (!str || !str[0])
+    return false;
+  int len = length(str);
+  for (int i = 0; i < len/2; ++i) {
+    if (str[i] != str[len - 1 - i])
+      return false;
+  }
+  return true;
+}
+
+int main() {
+  assert(ispalindrome("tenet") == true);
+  assert(ispalindrome("") == false);
+  assert(ispalindrome("hello world") == false);
+  assert(ispalindrome(nullptr) == false);
+  return 0;
 }
