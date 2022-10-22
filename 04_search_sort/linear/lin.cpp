7531718a6ccab4b4c040a0682a4aee7d341ec0cd
78chip <chepinoga.aa@edu.spbstu.ru>
2022-10-22 12:47:59 +0300

    feat: lin.cpp added

---
+++ 04_search_sort/linear/lin.cpp
@@ -0,0 +1,28 @@
+#include"linfunc.cpp"
+
+
+
+int main() 
+{
+    int x;
+    int arr[] = {11, 222, 55, 55, 55};
+    cout << "Element:" << endl;
+    cin >> x;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result[] = { 11, 222, 55, 55, 55 };
+
+    int num = count(arr, n, x, result);
+    if (num == -1)
+    {
+        cout << "Element is not present in array" << endl;
+    }
+    else
+    {
+        cout << "There are " << num << " elements" << endl;
+        cout << "Their idexes are " << endl;
+        for (int s = 0; s < num; s++)
+            cout << result[s] << endl;
+    }
+    system("pause");
+    return 0;
+}
