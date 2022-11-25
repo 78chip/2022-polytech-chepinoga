7fe1ee78907c9801c3316cb6aed038b83ae75e24
78chip <chepinoga.aa@edu.spbstu.ru>
2022-11-25 23:28:49 +0300

    fix: palind.cpp updated

--- 06_pointers_references/palindrome/palind.cpp
+++ 06_pointers_references/palindrome/palind.cpp
@@ -1,29 +1,20 @@
 #include <cassert>
 
 unsigned int length(const char* str) {
-  if (!str)
-    return 0;
-  const char* end = str;
-  for( ; *end != '\0'; ++end)
-    ;
-  return end - str;
-}
-
-bool ispalindrome(const char* str) {
-  if (!str || !str[0])
-    return false;
-  int len = length(str);
-  for (int i = 0; i < len/2; ++i) {
-    if (str[i] != str[len - 1 - i])
-      return false;
-  }
-  return true;
+    if (!str)
+        return 0;
+    const char* end = str;
+    for (; *end != '\0'; ++end)
+        ;
+    return end - str;
 }
 
 int main() {
-  assert(ispalindrome("tenet") == true);
-  assert(ispalindrome("") == false);
-  assert(ispalindrome("hello world") == false);
-  assert(ispalindrome(nullptr) == false);
-  return 0;
+    assert(length("a") == 1);
+    assert(length("ab") == 2);
+    assert(length("") == 0);
+    assert(length("hello world") == 11);
+    assert(length(nullptr) == 0);
+    return 0;
 }
+
